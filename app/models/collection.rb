20df1c392b4dbebc0211d8d257c2b50f789f7df2
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-07-28 03:49:31 -0400

    Add get_category association callback to Collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,9 @@
 class Collection < ApplicationRecord
-  has_many :skins
+  has_many :skins, before_add: :get_category
+
+  private
+
+  def get_category(skin)
+    update(category: skin.category)
+  end
 end

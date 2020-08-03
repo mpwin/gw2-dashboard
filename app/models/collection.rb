378f2c7d54712f3055f23ded4d63c9dd0dcf04ea
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-08-03 16:44:45 -0400

    Set collection weight_class when adding skin

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,9 +1,9 @@
 class Collection < ApplicationRecord
-  has_many :skins, before_add: :get_category
+  has_many :skins, before_add: :get_skin_attributes
 
   private
 
-  def get_category(skin)
-    update(category: skin.category)
+  def get_skin_attributes(skin)
+    update(category: skin.category, weight_class: skin.weight_class)
   end
 end

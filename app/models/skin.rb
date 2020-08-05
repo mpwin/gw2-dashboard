f3089ae2febc6c85fc319967d989cf3a0e382a5a
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-08-05 18:40:02 -0400

    Set collection unlock after skin save

--- app/models/skin.rb
+++ app/models/skin.rb
@@ -3,4 +3,14 @@ class Skin < ApplicationRecord
 
   serialize :flags,        Array
   serialize :restrictions, Array
+
+  after_save :set_collection_unlock, if: [:saved_change_to_unlocked, :collection]
+
+  def set_collection_unlock
+    if collection.skins.pluck(:unlocked).all?(true)
+      collection.update!(unlocked: true)
+    else
+      collection.update!(unlocked: false)
+    end
+  end
 end

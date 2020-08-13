e3606b9bdc840bb2e1ac00f2267e6f3f26dd079e
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-08-13 18:26:58 -0400

    Add test for Collection associations

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Collection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:skins) }
+  end
 end

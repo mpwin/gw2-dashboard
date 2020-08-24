3f7b10408b579c3da4144c9a1813dfc12518fbaf
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-08-24 16:39:57 -0400

    Add Skin model tests

--- spec/models/skin_spec.rb
+++ spec/models/skin_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Skin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should serialize(:flags) }
+  it { should serialize(:restrictions) }
+
+  describe 'associations' do
+    it { should belong_to(:collection).optional }
+
+    it 'updates its collection when its unlocked field is changed' do
+      skin = Skin.create(unlocked: false)
+      skin.create_collection
+      expect(skin.collection.unlocked).to eq(false)
+
+      skin.update(unlocked: true)
+      expect(skin.collection.unlocked).to eq(true)
+    end
+  end
 end

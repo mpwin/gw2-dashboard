b31de4415a4e1006762716a5ced91fda10899670
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-08-25 10:10:15 -0400

    Refactor Skin spec

--- spec/models/skin_spec.rb
+++ spec/models/skin_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Skin, type: :model do
+  subject { Skin.create(api_id: 1) }
+
   it { should serialize(:flags) }
   it { should serialize(:restrictions) }
 
   describe 'associations' do
     it { should belong_to(:collection).optional }
 
-    it 'updates its collection when its unlocked field is changed' do
-      skin = Skin.create(api_id: 1, unlocked: false)
-      skin.create_collection
-      expect(skin.collection.unlocked).to eq(false)
-
-      skin.update(unlocked: true)
-      expect(skin.collection.unlocked).to eq(true)
+    it 'updates its collection when :unlocked is changed' do
+      subject.create_collection
+      expect(subject.collection.unlocked).to eq(false)
+      subject.update(unlocked: true)
+      expect(subject.collection.unlocked).to eq(true)
     end
   end
 

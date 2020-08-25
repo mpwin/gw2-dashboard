e5f8377d9c17a0cb92ca50df1a3a5eca4df5d4e3
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-08-25 10:00:10 -0400

    Refactor Collection spec

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -4,50 +4,36 @@ RSpec.describe Collection, type: :model do
   describe 'associations' do
     it { should have_many(:skins) }
 
-    it 'raises an error when adding a skin of a different category' do
-      collection = Collection.new(category: 'Weapon')
-      skin       = Skin.new(category: 'Armor')
-  
-      expect { collection.skins << skin }.to raise_error("Category mismatch")
+    it 'sets :category on initial skin association' do
+      subject.skins << Skin.new(category: 'Weapon')
+      expect(subject.category).to eq('Weapon')
     end
 
-    it 'raises an error when adding a skin of a different weight class' do
-      collection = Collection.new(category: 'Armor', weight_class: 'Heavy')
-      skin       = Skin.new(category: 'Armor', weight_class: 'Light')
+    it 'sets :weight_class on initial skin association' do
+      subject.skins << Skin.new(category: 'Armor', weight_class: 'Heavy')
+      expect(subject.weight_class).to eq('Heavy')
+    end
 
-      expect { collection.skins << skin }.to raise_error("Weight class mismatch")
+    it 'updates :unlocked after adding a skin' do
+      subject.skins << Skin.new(unlocked: true)
+      expect(subject.unlocked).to eq(true)
+    end
+
+    it 'raises an error when adding a skin of a different :category' do
+      subject.category = 'Weapon'
+      skin = Skin.new(category: 'Armor')
+      expect { subject.skins << skin }.to raise_error('Category mismatch')
+    end
+
+    it 'raises an error when adding a skin of a different :weight_class' do
+      subject.weight_class = 'Heavy'
+      skin = Skin.new(category: 'Armor', weight_class: 'Light')
+      expect { subject.skins << skin }.to raise_error('Weight class mismatch')
     end
   end
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:category) }
-  end
-
-  it 'sets its category field on initial skin association' do
-    collection = Collection.new
-    skin       = Skin.new(category: 'Weapon')
-
-    collection.skins << skin
-
-    expect(collection.category).to eq('Weapon')
-  end
-
-  it 'sets its weight class field on initial skin association' do
-    collection = Collection.new
-    skin       = Skin.new(category: 'Armor', weight_class: 'Heavy')
-
-    collection.skins << skin
-
-    expect(collection.weight_class).to eq('Heavy')
-  end
-
-  it 'updates its unlocked field after adding a skin' do
-    collection = Collection.new
-    skin       = Skin.new(unlocked: true)
-
-    collection.skins << skin
-
-    expect(collection.unlocked).to eq(true)
   end
 end

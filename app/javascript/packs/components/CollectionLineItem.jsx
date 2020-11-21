b2ad89aebaac27c0dbeef470ae8b46157b3dd2d5
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-11-21 18:25:00 -0500

    Refactor CollectionLineItem classNames

--- app/javascript/packs/components/CollectionLineItem.jsx
+++ app/javascript/packs/components/CollectionLineItem.jsx
@@ -7,8 +7,26 @@ class CollectionLineItem extends React.Component {
     this.state = { hover: false };
   }
 
+  getClass = () => {
+    if (this.isSelected()) {
+      return 'list-group-item-primary';
+    }
+
+    if (this.state.hover) {
+      return this.props.collection.unlocked ? 'list-group-item-light' : 'list-group-item-dark';
+    }
+
+    if (this.props.collection.unlocked) {
+      return 'list-group-item-success';
+    }
+  }
+
   handleClick = (collection) => {
     this.props.selectCollection(collection);
+  }
+
+  isSelected = () => {
+    return this.props.collection == this.props.selectedCollection;
   }
 
   setHover = (hover) => {
@@ -16,14 +34,7 @@ class CollectionLineItem extends React.Component {
   }
 
   render() {
-    let selected = this.props.collection == this.props.selectedCollection;
-    let liClass  = classNames(
-      'list-group-item', { 'cursor-pointer': !selected },
-      { 'list-group-item-primary': selected },
-      { 'list-group-item-success': ( this.props.collection.unlocked && !this.state.hover && !selected) },
-      { 'list-group-item-light':   ( this.props.collection.unlocked &&  this.state.hover && !selected) },
-      { 'list-group-item-dark':    (!this.props.collection.unlocked &&  this.state.hover && !selected) }
-    );
+    let liClass = classNames('list-group-item', this.getClass(), { 'cursor-pointer': !this.isSelected() });
 
     return(
       <li className={liClass} onClick={() => this.handleClick(this.props.collection)} onMouseEnter={() => this.setHover(true)} onMouseLeave={() => this.setHover(false)}>

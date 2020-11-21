4bd47630b928104b12940364d9d0a0aeb965f351
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-11-21 16:21:57 -0500

    Add hover state to CollectionLineItem component

--- app/javascript/packs/components/CollectionLineItem.jsx
+++ app/javascript/packs/components/CollectionLineItem.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import classNames from 'classnames';
 
 class CollectionLineItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hover: false };
+  }
+
   handleClick = (collection) => {
     this.props.showCollection(collection);
+  }
+
+  setHover = (hover) => {
+    this.setState({ hover: hover });
   }
 
   render() {
     let liClass = classNames(
       'list-group-item',
-      { 'list-group-item-success': this.props.collection.unlocked }
+      { 'list-group-item-success': ( this.props.collection.unlocked && !this.state.hover) },
+      { 'list-group-item-light':   ( this.props.collection.unlocked &&  this.state.hover) },
+      { 'list-group-item-dark':    (!this.props.collection.unlocked &&  this.state.hover) }
     );
 
     return(
-      <li className={liClass} onClick={this.handleClick.bind(this, this.props.collection)}>
+      <li className={liClass} onClick={this.handleClick.bind(this, this.props.collection)} onMouseEnter={this.setHover.bind(this, true)} onMouseLeave={this.setHover.bind(this, false)}>
         {this.props.collection.name}
       </li>
     );

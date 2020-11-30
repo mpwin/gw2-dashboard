fcc375487bb94f4f8ec68621fdf6f8e271abc683
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-11-30 17:29:26 -0500

    Render outfits in Outfits component

--- app/javascript/packs/components/Outfits.jsx
+++ app/javascript/packs/components/Outfits.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import classNames from 'classnames';
 
 class Outfits extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { outfits: [] };
+  }
+
+  componentDidMount() {
+    fetch('/api/outfits.json')
+      .then((response) => { return response.json() })
+      .then((data)     => { this.setState({ outfits: data }) });
+  }
+
   render() {
+    var outfits = this.state.outfits.map((outfit) => {
+      return (
+        <li className={classNames('list-group-item', { 'list-group-item-success': outfit.unlocked })}>
+          {outfit.name}
+        </li>
+      );
+    });
+
     return (
-      <div>Outfits</div>
+      <div className='container-fluid mt-4'>
+        <div className='row'>
+          <div className='col-2'>
+            <ul className='list-group text-monospace'>
+              <li className='list-group-item active'>Outfits</li>
+              {outfits}
+            </ul>
+          </div>
+        </div>
+      </div>
     );
   }
 }

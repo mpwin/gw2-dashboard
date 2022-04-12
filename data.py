56c5f41935a0093588a913c2a3eb854fd06d5765
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-04-12 15:07:25 -0400

    Begin storing data from api

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
+import redis
 import requests
 import time
 
+red = redis.Redis()
 url = 'https://api.guildwars2.com/v2/skins'
 res = requests.get(url)
 ids = list(res.json())
@@ -10,6 +12,9 @@ for id in range(0, len(ids), 200):
     res = requests.get(url, params = { 'ids': ','.join(map(str, grp)) })
 
     for skin in res.json():
-        print(f"{skin['id']} | {skin['name']}")
+        if not skin['name']: continue
+        red.sadd('skin_ids', skin['id'])
+        red.hset('skin:%s' %(skin['id']), 'name', skin['name'])
+        print('%s | %s' %(skin['id'], skin['name']))
 
     time.sleep(2)

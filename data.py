de8b4d62ffeed325169ad024c1754f27c3b822fc
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-05-26 17:04:05 -0400

    Clean up get_data()

--- data.py
+++ data.py
@@ -7,45 +7,41 @@ from operator import itemgetter
 
 r = redis.Redis()
 
-
 def get_data():
-    red = redis.Redis()
+    url = 'https://api.guildwars2.com/v2'
 
-    url = 'https://api.guildwars2.com/v2/account/skins'
     with open('key.txt') as f:
         key = f.readline().rstrip()
-    res = requests.get(url, params={'access_token': key})
+    res = requests.get(f'{url}/account/skins', params={'access_token': key})
     ids = list(res.json())
 
-    for id in ids:
-        red.sadd('unlocked_skin_ids', id)
+    for i in ids:
+        r.sadd('unlocked_skin_ids', i)
 
-    print(ids)
     time.sleep(2)
 
-    url = 'https://api.guildwars2.com/v2/skins'
-    res = requests.get(url)
+    res = requests.get(f'{url}/skins')
     ids = list(res.json())
 
-    for id in range(0, len(ids), 200):
-        grp = ids[id:id + 200]
-        res = requests.get(url, params={'ids': ','.join(map(str, grp))})
+    for i in range(0, len(ids), 200):
+        grp = ids[i:i + 200]
+        res = requests.get(f'{url}/skins', params={'ids': ','.join(map(str, grp))})
 
         for skin in res.json():
             if not skin['name']: continue
-            red.sadd('skin_ids', skin['id'])
-            red.hset('skin:%s' %(skin['id']), 'name', skin['name'])
+            r.sadd('skin_ids', skin['id'])
+            r.hset('skin:%s' %(skin['id']), 'name', skin['name'])
 
             if skin['type'] == 'Weapon':
-                red.sadd('weapon_ids', skin['id'])
+                r.sadd('weapon_ids', skin['id'])
             if skin['type'] == 'Armor':
                 match skin['details']['weight_class']:
                     case 'Heavy':
-                        red.sadd('heavy_armor_ids', skin['id'])
+                        r.sadd('heavy_armor_ids', skin['id'])
                     case 'Medium':
-                        red.sadd('medium_armor_ids', skin['id'])
+                        r.sadd('medium_armor_ids', skin['id'])
                     case 'Light':
-                        red.sadd('light_armor_ids', skin['id'])
+                        r.sadd('light_armor_ids', skin['id'])
 
             print('%s | %s' %(skin['id'], skin['name']))
 
@@ -60,6 +56,8 @@ def create_collections():
         for index, collection in enumerate(data[category]):
             r.sadd(f'collections:{category}', index)
             r.set(f'collection:{category}:{index}', collection['name'])
+
+
 
 
 def collection_list(category):

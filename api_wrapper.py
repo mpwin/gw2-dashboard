faad6a32f10520f367cf7048078b23d0e99055ca
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-06-20 17:40:44 -0400

    Refactor PageIterator instance methods

--- api_wrapper.py
+++ api_wrapper.py
@@ -35,28 +35,24 @@ class Wrapper:
 
             def __init__(self, endpoint):
                 self.endpoint = endpoint
-                self.page_total = self._get_page_total(endpoint)
                 self.page = 0
+                self.page_total = self._get_page_total()
 
             def __iter__(self):
                 return self
 
             def __next__(self):
                 if self.page < self.page_total:
-                    response = requests.get(self.endpoint, params=self._params(page=self.page))
+                    response = requests.get(self.endpoint, params=self._params())
                     self.page += 1
                     time.sleep(2)
                     return response.json()
                 else:
                     raise StopIteration
 
-            def _get_page_total(self, endpoint):
-                response = requests.get(endpoint, params=self._params())
+            def _get_page_total(self):
+                response = requests.get(self.endpoint, params=self._params())
                 return int(response.headers.get('X-Page-Total'))
 
-            def _params(self, **keys):
-                params = {
-                    'page': keys.get('page') or 0,
-                    'page_size': keys.get('page_size') or 200,
-                    }
-                return params
+            def _params(self):
+                return {'page': self.page, 'page_size': 200}

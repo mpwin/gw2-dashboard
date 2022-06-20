b10480b97af098e167ca07a1ad2be3afe8f84a7f
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-06-20 12:15:25 -0400

    Add ObjectIterator

--- api_wrapper.py
+++ api_wrapper.py
@@ -7,35 +7,51 @@ class Wrapper:
         self.url = 'https://api.guildwars2.com/v2/'
 
     def skins(self):
-        endpoint = self.url + 'skins'
-        return self.PageIterator(endpoint)
+        return self.ObjectIterator(self.url + 'skins')
 
 
-    class PageIterator:
+    class ObjectIterator:
         def __init__(self, endpoint):
-            self.endpoint = endpoint
-            self.page_total = _get_page_total(endpoint)
-            self.page = 0
+            self.objects = []
+            self.page = self.PageIterator(endpoint)
 
         def __iter__(self):
             return self
 
         def __next__(self):
-            if self.page >= self.page_total:
+            if not self.objects:
+                self.objects = next(self.page)
+
+            if self.objects:
+                return self.objects.pop(0)
+            else:
                 raise StopIteration
-            response = requests.get(self.endpoint, params=_params(page=self.page))
-            self.page += 1
-            return response.json()
 
 
-def _get_page_total(endpoint):
-    response = requests.get(endpoint, params=_params())
-    return int(response.headers.get('X-Page-Total'))
+        class PageIterator:
+            def __init__(self, endpoint):
+                self.endpoint = endpoint
+                self.page_total = self._get_page_total(endpoint)
+                self.page = 0
 
+            def __iter__(self):
+                return self
 
-def _params(**keys):
-    params = {
-        'page': keys.get('page') or 0,
-        'page_size': keys.get('page_size') or 200,
-        }
-    return params
+            def __next__(self):
+                if self.page < self.page_total:
+                    response = requests.get(self.endpoint, params=self._params(page=self.page))
+                    self.page += 1
+                    return response.json()
+                else:
+                    raise StopIteration
+
+            def _get_page_total(self, endpoint):
+                response = requests.get(endpoint, params=self._params())
+                return int(response.headers.get('X-Page-Total'))
+
+            def _params(self, **keys):
+                params = {
+                    'page': keys.get('page') or 0,
+                    'page_size': keys.get('page_size') or 200,
+                    }
+                return params

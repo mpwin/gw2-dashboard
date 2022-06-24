30b2b469ababfd983b1a47aa1891db04bc75f79c
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-06-24 17:12:27 -0400

    Add fixtures for test data

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from unittest.mock import patch
 
@@ -6,22 +7,24 @@ import data_redis as data
 
 class TestData(unittest.TestCase):
 
-    @patch('data_redis.db')
-    def test_skin_init(self, mock_db):
-        mock_db.get.return_value = {
-            'name': 'Seer Pants',
-            'type': 'Leggings',
-            }
-        skin = data.Skin(10)
-        self.assertIsInstance(skin, data.Skin)
-        self.assertEqual(skin.name, 'Seer Pants')
-        self.assertEqual(skin.type, 'Leggings')
+    def get_data(self, name, _id):
+        with open('tests/fixtures/test_data.json') as f:
+            data = json.load(f)
+        return data[name][str(_id)]
 
-    @patch('data_redis.db')
-    def test_class_get(self, mock_db):
-        skins = data.Skin.get('weapon')
-        self.assertIsInstance(skins, list)
-        self.assertTrue(all(isinstance(i, data.Skin) for i in skins))
+    def test_skin_init(self):
+        with patch('data_redis.db') as mock_db:
+            mock_db.get.side_effect = self.get_data
+            skin = data.Skin(10)
+            self.assertIsInstance(skin, data.Skin)
+            self.assertEqual(skin.name, 'Seer Pants')
+            self.assertEqual(skin.type, 'Leggings')
+
+    def test_class_get(self):
+        with patch('data_redis.db') as mock_db:
+            skins = data.Skin.get('weapon')
+            self.assertIsInstance(skins, list)
+            self.assertTrue(all(isinstance(i, data.Skin) for i in skins))
 
 
 if __name__ == '__main__':

2cbde006de38c06cab3ea31db12656f6a98f96b8
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-06-24 14:20:01 -0400

    Begin using unittest mock and patch

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,24 @@
 import unittest
+from unittest.mock import patch
 
 import data_redis as data
 
 
 class TestData(unittest.TestCase):
 
-    def test_skin_init(self):
+    @patch('data_redis.db')
+    def test_skin_init(self, mock_db):
+        mock_db.get.return_value = {
+            'name': 'Seer Pants',
+            'type': 'Leggings',
+            }
         skin = data.Skin(10)
         self.assertIsInstance(skin, data.Skin)
         self.assertEqual(skin.name, 'Seer Pants')
         self.assertEqual(skin.type, 'Leggings')
 
-    def test_class_get(self):
+    @patch('data_redis.db')
+    def test_class_get(self, mock_db):
         skins = data.Skin.get('weapon')
         self.assertIsInstance(skins, list)
         self.assertTrue(all(isinstance(i, data.Skin) for i in skins))

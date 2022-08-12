c23eafa859b4a812ab6148941170a1e92fc802e7
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-08-12 10:36:01 -0400

    Add __main__ guard to setup module

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ import data
 api = api_wrapper.Wrapper()
 
 
-def fetch():
+def setup():
     fetch_skins()
 
 
 def fetch_skins():
     for skin in api.skins():
         data.Skin.save(skin)
+
+
+if __name__ == '__main__':
+    setup()

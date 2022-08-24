357c87bdb2c41f7a7574216ff966851f159dabd2
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-08-24 15:04:31 -0400

    Print progress of setup.py to console

--- setup.py
+++ setup.py
@@ -3,18 +3,32 @@ import data
 
 
 def setup():
+    print('Running setup...')
+
     fetch_skins()
     fetch_account_skins()
     data.skins.setup()
 
+    print('Setup complete.')
+
 
 def fetch_skins():
+    print('Fetching skins...')
+
     for skin in api.skins():
+        print(f"\tSkin {skin.get('id')} : {skin.get('name')}")
         data.skins.save(skin)
+
+    print('Skins fetched.')
 
 
 def fetch_account_skins():
-    data.skins.save_unlocked(api.account_skins())
+    print('Fetching account skins...', end=' ')
+
+    unlocked = api.account_skins()
+    data.skins.save_unlocked(unlocked)
+
+    print(f'{len(unlocked)} skins unlocked.')
 
 
 if __name__ == '__main__':

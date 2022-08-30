769c3b4fa0d06eb10c6ce41dd96c8966b30c7af8
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2022-08-30 14:23:49 -0400

    Rename db to r for more clarity when calling redis directly

--- data/db.py
+++ data/db.py
@@ -1,41 +1,41 @@
 import redis
 
 
-db = redis.Redis(decode_responses=True)
+r = redis.Redis(decode_responses=True)
 
 
 def get(scope, _id):
-    return db.hgetall(':'.join((scope, _id)))
+    return r.hgetall(':'.join((scope, _id)))
 
 
 def get_set(scope, name):
     name = ':'.join((scope, name))
 
-    match db.type(name):
+    match r.type(name):
         case 'set':
-            return db.smembers(name)
+            return r.smembers(name)
         case 'zset':
-            return db.zrange(name, 0, -1)
+            return r.zrange(name, 0, -1)
 
 
 def in_set(scope, name, data):
-    return db.sismember(f'{scope}:{name}', data)
+    return r.sismember(f'{scope}:{name}', data)
 
 
 def save(scope, _id, data):
-    db.hmset('%s:%s' %(scope, _id), data)
+    r.hmset('%s:%s' %(scope, _id), data)
 
 
 def add_to_set(scope, name, *data):
-    db.sadd(f'{scope}:{name}', *data)
+    r.sadd(f'{scope}:{name}', *data)
 
 
 def add_to_sorted_set(name, _id, score=None):
     if not score:
         score = _id
-    db.zadd(name, {_id: score})
+    r.zadd(name, {_id: score})
 
 
 def create_sorted_set(scope, name, sets):
-    _list = sorted(db.sinter(sets), key=lambda i: get(scope, i)['name'])
-    db.zadd(name, {i: score for score, i in enumerate(_list)})
+    _list = sorted(r.sinter(sets), key=lambda i: get(scope, i)['name'])
+    r.zadd(name, {i: score for score, i in enumerate(_list)})
